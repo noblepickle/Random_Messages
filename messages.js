6a1f5b78b408e26d230861efb4dcb9094c388abd
thenoblepickle <aubrunnerkorbinian3@gmail.com>
2025-12-29 11:30:46 +0100

    Add function pickRandomString().

--- messages.js
+++ messages.js
@@ -1,6 +1,11 @@
 //set up strings
 const str1 = ['You should', 'You could', 'You have to'];
 const str2 = ['buy', 'get', 'listen to'];
-const str3 = ['flower', 'yourself', 'me'];
+const str3 = ['flowers', 'yourself', 'me'];
 
 //pick a random message
+const pickRandomString = arr => {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+//console.log(pickRandomString(str2));
+
