7a2494b069392d0575d30facda54f7723d37233f
thenoblepickle <aubrunnerkorbinian3@gmail.com>
2025-12-29 11:21:50 +0100

    initial upload.

---
+++ messages.js
@@ -0,0 +1,6 @@
+//set up strings
+const str1 = ['You should', 'You could', 'You have to'];
+const str2 = ['buy', 'get', 'listen to'];
+const str3 = ['flower', 'yourself', 'me'];
+
+//pick a random message
